9c12050349e8a04ebb146622d925decdc5848644
Alex Flint <alex.flint@gmail.com>
2021-12-23 09:40:04 -0500

    update enable-orbi-telnet.sh

--- scripts/enable-orbi-telnet.sh
+++ scripts/enable-orbi-telnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 netgear-enable-telnet.py 10.0.0.1 78D2949BF32B admin clue17handle
+python3 enable-orbi-telnet.py 10.0.0.1 78D2949BF32B admin clue17handle
--- scripts/netgear-enable-telnet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 enable-orbi-telnet.py 10.0.0.1 78D2949BF32B admin clue17handle
