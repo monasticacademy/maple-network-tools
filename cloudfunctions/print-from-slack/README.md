932abdc8198ae4a8b8fb49fa0a52ae6e7877bb79
Alex Flint <alex.flint@gmail.com>
2022-07-21 11:36:06 -0400

    add a cloudfunction for a slack app

---
+++ cloudfunctions/print-from-slack/README.md
@@ -0,0 +1,1 @@
+This directory contains a cloud function that is the endpoint for a Slack App named "print from slack". It prints all attachments posted to the slack channel "#print" to the Brother printer at MAPLE
---
+++ cloudfunctions/print-from-slack/print-from-slack.go
@@ -0,0 +1,68 @@
+// Package p contains an HTTP Cloud Function.
+package p
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+)
+
+var (
+	pubsubClient *pubsub.Client
+)
+
+var (
+	project = os.Getenv("PROJECT")
+	topic   = os.Getenv("PUBSUB_TOPIC")
+	token   = os.Getenv("SLACK_TOKEN")
+	secret  = os.Getenv("SLACK_SIGNING_SECRET")
+)
+
+func init() {
+	var err error
+	ctx := context.Background()
+	pubsubClient, err = pubsub.NewClient(ctx, project)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// PrintGoogleDoc is the entrypoint for the cloud function
+func PrintGoogleDoc(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	document := r.URL.Query().Get("document") // a google doc ID
+	if document == "" {
+		http.Error(w,
+			"missing 'document' URL parameter",
+			http.StatusBadRequest)
+		return
+	}
+
+	msg := map[string]string{"document": document}
+	buf, err := json.Marshal(msg)
+	if err != nil {
+		http.Error(w,
+			"error marshalling json: "+err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	topic := pubsubClient.Topic(topic)
+	promise := topic.Publish(ctx, &pubsub.Message{
+		Data: buf,
+	})
+
+	_, err = promise.Get(ctx)
+	if err != nil {
+		http.Error(w,
+			"error pushing message to pubsub: "+err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "submitted document %s to the pubsub topic\n", document)
+}
